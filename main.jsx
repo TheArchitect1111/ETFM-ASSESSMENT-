90942b5b082fad42a301bdacb371dbbeb99e15ac
Robert Brickey <brickmail1@gmail.com>
2026-07-10 23:52:12 -0400

    Resolve application routing conflict

--- main.jsx
+++ main.jsx
@@ -1,20 +1,12 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import App from './src/App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-=======
-import ETFMAssessment from './ETFMAssessment.jsx'
 import FormToFinishReview from './FormToFinishReview.jsx'
 
-const CurrentApp = window.location.pathname.startsWith('/form-to-finish-review') ? FormToFinishReview : ETFMAssessment
+const CurrentApp = window.location.pathname.startsWith('/form-to-finish-review') ? FormToFinishReview : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CurrentApp />
->>>>>>> 26f877a (Add Dylan Debono executive review)
   </React.StrictMode>
 )
