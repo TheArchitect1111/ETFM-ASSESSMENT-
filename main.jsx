e666dad6ad7d8edfce2297551ae758f55c76a19d
Robert Brickey <brickmail1@gmail.com>
2026-07-10 23:38:16 -0400

    Add Dylan Debono executive review

--- main.jsx
+++ main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+<<<<<<< HEAD
 import App from './src/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+=======
+import ETFMAssessment from './ETFMAssessment.jsx'
+import FormToFinishReview from './FormToFinishReview.jsx'
+
+const CurrentApp = window.location.pathname.startsWith('/form-to-finish-review') ? FormToFinishReview : ETFMAssessment
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CurrentApp />
+>>>>>>> 26f877a (Add Dylan Debono executive review)
   </React.StrictMode>
 )
